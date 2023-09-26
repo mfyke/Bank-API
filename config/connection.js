5ea41c10e0280171f21923a52db1ad9a218fefb3
mfyke <mfyke@live.com>
2023-09-26 15:04:09 -0700

    update to postgres

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
-      dialect: 'mysql',
+      dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
       },
