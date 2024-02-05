2ba893c3d157015d6a2a4644b385af5fec609882
mfyke <mfyke@live.com>
2024-02-05 12:17:35 -0800

    update connection to not use hosted db

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,22 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'postgres',
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+});
 
 module.exports = sequelize;
